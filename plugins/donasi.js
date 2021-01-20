a4f64080a5a7a4c1a5c26fd18285400527f7f23d
自己做饭 <73934059+yahtoiba@users.noreply.github.com>
2021-01-20 16:55:12 +0700

    Update donasi.js

--- plugins/donasi.js
+++ plugins/donasi.js
@@ -1,13 +1,14 @@
 let handler = async m => m.reply(`
-╔═──「 Mau • Donasikah? 」
+╔═──「 Mao • Donasikah? 」
 │------------
 │ ❏ OVO  : 0877-1474-5440
 │ ❏ DANA : 0877-1474-5440
 │ ❏ PULSA: 0877-1474-5440
 │------------
-│ ❏ Mao donasi? bit.ly/ClickDiSinii
-│ ❏ Atw? saweria.co/PUDlDl
-╚═────────────────────────────
+│ ❏ Atw Bisa klik bit.ly/ClickDiSinii
+│ ❏ Atw juga inihh hhe saweria.co/PUDlDl
+│ ❏ Maaci>< mwhhehhehehehehehehehhe
+╚═─────────────────────────────────ᜰ
 `.trim()) // Tambah sendiri kalo mau
 handler.help = ['donasi']
 handler.tags = ['about']
