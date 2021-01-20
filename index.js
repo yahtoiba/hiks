ba460d5e855c0e3885f987cd8f8867eb55eee7a7
自己做饭 <73934059+yahtoiba@users.noreply.github.com>
2021-01-20 09:56:26 +0700

    Update index.js

--- index.js
+++ index.js
@@ -147,10 +147,10 @@ conn.handler = async function (m) {
 
         m.isCommand = true
         let xp = 'exp' in plugin ? parseInt(plugin.exp) : 9
-        if (xp > 99) m.reply('Ngecit -_-')
+        if (xp > 99) m.reply('Ngecheat njim -_-')
         else m.exp += xp
         if (!isPrems && global.DATABASE._data.users[m.sender].limit < 1 && plugin.limit) {
-          this.reply(m.chat, `Limit anda habis, silahkan beli melalui *${usedPrefix}buy*`, m)
+          this.reply(m.chat, `yahh Limit kamu habis:(, silahkan beli melalui *${usedPrefix}buy*`, m)
           continue
         }
         try {
