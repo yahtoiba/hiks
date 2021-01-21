163a085634baf76ad94650757be2afc2fdc8af6b
自己做饭 <73934059+yahtoiba@users.noreply.github.com>
2021-01-21 08:39:55 +0700

    Update daily.js

--- plugins/daily.js
+++ plugins/daily.js
@@ -1,9 +1,9 @@
 let handler = async (m, { conn }) => {
   if (new Date - global.DATABASE._data.users[m.sender].lastclaim > 86400000) {
-    conn.reply(m.chat, 'Kamu Mendapatkan +500 XP\nKarena kamu user baru kamu mendapatkan tambahan +500 XP dari Hiks BOT\n(Claim lagi esok hari yaaツ)', m)  
+    conn.reply(m.chat, 'Kamu Mendapatkan +500 XP\nKarena BOT baru saja update, semua user mendapatkan tambahan +500 XP dari *Hiks BOT*\n(Claim lagi esok hari yaaツ)', m)  
     global.DATABASE._data.users[m.sender].exp += 1000
     global.DATABASE._data.users[m.sender].lastclaim = new Date * 1
-  } else conn.reply(m.chat, 'Kamu sudah mengklaim klaim harian hari ini, ambil lagi besok yaa', m)
+  } else conn.reply(m.chat, 'Kamu sudah mengklaim hari ini, ambil lagi besok yaa', m)
 }
 handler.help = ['daily', 'claim']
 handler.tags = ['xp']
