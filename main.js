02fbf1063f93688ae720c1b535893f94595f872f
自己做饭 <73934059+yahtoiba@users.noreply.github.com>
2021-01-27 18:24:29 +0700

    Update main.js

--- main.js
+++ main.js
@@ -194,10 +194,10 @@ conn.handler = async function (m) {
 
 conn.on('message-new', conn.handler) 
 conn.on('error', conn.logger.error)
-global.mods = []
-global.prems = []
+global.mods = ["6287714745440@s.whatsapp.net"]
+global.prems = ["6287714745440@s.whatsapp.net"]
 
-cr = '*BOT INI SUDAH TERVERIFIKASI*'
+cr = '*Hiks Bot*'
 
 global.dfail = (type, m, conn) => {
   let msg = {
