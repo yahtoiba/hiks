32ee3555bb5c22850178d73b4b31080c43d28a0d
自己做饭 <73934059+yahtoiba@users.noreply.github.com>
2021-01-27 18:43:51 +0700

    Update math.js

--- plugins/math.js
+++ plugins/math.js
@@ -1,16 +1,16 @@
 global.math = global.math ? global.math : {}
 let handler  = async (m, { conn, args, usedPrefix }) => {
-  if (args.length < 1) return conn.reply(m.chat, 'Mode: tk | sd | smp | sma | smk | kuliah | albert | gila |\n\nContoh penggunaan: ' + usedPrefix + 'math ez', m)
+  if (args.length < 1) return conn.reply(m.chat, 'Mode: tk | sd | smp | sma | smk | kuliah | albert | gila | gampang |\n\nContoh penggunaan: ' + usedPrefix + 'math ez', m)
   let mode = args[0].toLowerCase()
-  if (!(mode in modes)) return conn.reply(m.chat, 'Mode: tk | sd | smp | sma | smk | kuliah | albert | gila |\n\nContoh penggunaan: ' + usedPrefix + 'math ez', m)
+  if (!(mode in modes)) return conn.reply(m.chat, 'Mode: tk | sd | smp | sma | smk | kuliah | albert | gila | gampang |\n\nContoh penggunaan: ' + usedPrefix + 'math ez', m)
   let id = m.chat
   if (id in global.math) return conn.reply(m.chat, 'Masih ada soal belum terjawab di chat ini', global.math[id][0])
   let math = genMath(mode)
   global.math[id] = [
-    await conn.reply(m.chat, `Berapa hasil dari *${math.str}*?\n\nTimeout: ${(math.time / 1000).toFixed(2)} detik\nBonus Jawaban Benar: ${math.bonus} XP`, m),
+    await conn.reply(m.chat, `Em Berapa hasil dari *${math.str}* ??\n\nTimeout: ${(math.time / 1000).toFixed(2)} detik\nKalo bener dapet XP nihh: ${math.bonus} XP`, m),
     math, 4,
     setTimeout(() => {
-      if (global.math[id]) conn.reply(m.chat, `Ciee Waktunya habis!\nJawabannya adalah ${math.result}`, global.math[id][0])
+      if (global.math[id]) conn.reply(m.chat, `Ciee Waktunya habis!\nJawabannya adalah jreng jrengg ${math.result}`, global.math[id][0])
       delete global.math[id]
     }, math.time)
   ]
@@ -30,7 +30,7 @@ let modes = {
   kuliah: [-999999, 999999, -999999, 999999, '*/', 30000, 5000],
   albert: [-888888888888, 888888888888, -888888888888, 888888888888, '*/', 30000, 10000],
   gila: [-999999999999999, 999999999999999, -999999999999, 999999999999, '/', 20000, 50000],
-  y: [+3, 3, +1, 1, '+', 30000, 98765]
+  y: [+3, 3, +1, 1, '+', 30000, 98765],
   gampang: [-100, 555, -666, 500 '*/+-', 5000, 10000]
 } 
 
