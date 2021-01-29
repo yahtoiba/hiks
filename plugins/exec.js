aae6faf0d38c0421ecc657ee90cb8494c87323db
自己做饭 <73934059+yahtoiba@users.noreply.github.com>
2021-01-29 07:57:27 +0700

    Update exec.js

--- plugins/exec.js
+++ plugins/exec.js
@@ -1,22 +1,19 @@
 let syntaxerror = require('syntax-error')
 let util = require('util')
 
-let handler  = async (m, { conn, usedPrefix, command, text, noPrefix, args, isOwner }) => {
+let handler  = async (m, { conn, usedPrefix, command, text, noPrefix, args, groupMetadata }) => {
   let _return
   let _syntax = ''
   let _text = (/^=/.test(usedPrefix) ? 'return ' : '') + noPrefix
-  let old = m.exp * 1
-  conn.clearAuthInfo = conn.groupLeave = () => {
-    throw '_Lu seneng kalo lu diginiin?_'
-  }
-  let i = 20
+  let old = m.exp * 1 
   try {
-    let exec = new (async () => {}).constructor('print', 'm', 'handler', 'require', 'conn', 'Array', 'process', 'args', 'global', _text)
+    let i = 15
+    let exec = new (async () => {}).constructor('print', 'm', 'handler', 'require', 'conn', 'Array', 'process', 'args', 'groupMetadata', _text)
     _return = await exec((...args) => {
       if (--i < 1) return
       console.log(...args)
       return conn.reply(m.chat, util.format(...args), m)
-    }, m, handler, require, m.fromMe ? conn : { ...conn, deleteChat: conn.clearAuthInfo }, CustomArray, {...process, exit: function exit() { return ':P' }}, args, Object.freeze({ ...global, global: 'Lu mau apa kesini -_-'}))
+    }, m, handler, require, conn, CustomArray, {...process, exit: function exit() { return ':P' }}, args, groupMetadata)
   } catch (e) {
     let err = await syntaxerror(_text)
     if (err) _syntax = '```' + err + '```\n\n'
@@ -30,6 +27,7 @@ handler.help = ['> ', '=> ']
 handler.tags = ['advanced']
 handler.customPrefix = /^=?> /
 handler.command = /(?:)/i
+handler.rowner = true
 handler.owner = false
 handler.mods = false
 handler.premium = false
